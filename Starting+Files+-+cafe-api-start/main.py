b7d6f13d53926ed2e58a9192d75fd6ddd8500037
AleemRahil <aleem.rahil@outlook.com>
2023-05-23 10:30:04 -0400

    rest api

--- Starting+Files+-+cafe-api-start/main.py
+++ Starting+Files+-+cafe-api-start/main.py
@@ -1,32 +1,31 @@
-from flask import Flask, jsonify, render_template, requests
+from flask import Flask, jsonify, render_template, request
+import requests
 from flask_sqlalchemy import SQLAlchemy
 import random
 
 app = Flask(__name__)
 
-##CREATE DB
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-
-##CREATE TABLE
+cafes = db.Table('cafe', autoload=True, autoload_width=db.engine)
 class Cafe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), unique=True, nullable=False)
     map_url = db.Column(db.String(500), nullable=False)
     img_url = db.Column(db.String(500), nullable=False)
     location = db.Column(db.String(250), nullable=False)
-    seats = db.Column(db.String(250), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
     has_toilet = db.Column(db.Boolean, nullable=False)
     has_wifi = db.Column(db.Boolean, nullable=False)
-    has_sockets = db.Column(db.Boolean, nullable=False)
     can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
     coffee_price = db.Column(db.String(250), nullable=True)
 
     def to_dict(self):
-        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
-
+        return {column.name: str(getattr(self, column.name)) for column in self.__table__.columns}
 
 @app.route("/")
 def home():
@@ -40,10 +39,16 @@ def get_random_cafe():
     return jsonify(cafe=random_cafe.to_dict())
 
 
+ 
 @app.route("/all")
 def get_all_cafes():
     cafes = db.session.query(Cafe).all()
     return jsonify(cafes=[cafe.to_dict() for cafe in cafes])
+
+
+
+
+
 
 # @app.route("/search")
 # def search_cafe_at_location(lat, lon):
